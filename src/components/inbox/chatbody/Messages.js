2b17af5d722983af720b3611f7cdfa2727ace9ee
khan188993 <khan188993@gmail.com>
2022-09-17 21:58:59 +0600

    messages body show done

--- src/components/inbox/chatbody/Messages.js
+++ src/components/inbox/chatbody/Messages.js
@@ -1,27 +1,23 @@
+import { useSelector } from "react-redux";
 import Message from "./Message";
 
-export default function Messages() {
+export default function Messages({ messages = [] }) {
+    const { user } = useSelector((state) => state.auth) || {};
+    const { email } = user || {};
+
     return (
         <div className="relative flex h-[calc(100vh_-_197px)] w-full flex-col-reverse overflow-y-auto p-6">
             <ul className="space-y-2">
-                <Message justify="start" message="Hjdjd" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
-                <Message justify="start" message="Hi" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
-                <Message justify="start" message="Hi" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
-                <Message justify="start" message="Hi" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
-                <Message justify="start" message="Hi" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
-                <Message justify="start" message="Hi" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
+                {messages
+                    .slice()
+                    .sort((a, b) => a.timestamp - b.timestamp)
+                    .map((message) => {
+                        const { message: lastMessage, id, sender } = message || {};
+
+                        const justify = sender.email !== email ? "start" : "end";
+
+                        return <Message key={id} justify={justify} message={lastMessage} />;
+                    })}
             </ul>
         </div>
     );
